f50266740b2c08379299824d015468d5263aa3d4
Alastair M. Robinson <robinsonb5@gmail.com>
2021-02-23 23:21:39 +0000

    Chameleon64 v1 target working.

--- Board/chameleon64/chameleon64_support.tcl
+++ Board/chameleon64/chameleon64_support.tcl
@@ -1,7 +1,5 @@
-set_global_assignment -name VHDL_FILE ${boardpath}/chameleon64/chameleon64_top.vhd
+set_global_assignment -name VHDL_FILE ${projectpath}/toplevels/chameleon64_top.vhd
 set_global_assignment -name QIP_FILE ${boardpath}/chameleon-modules/chameleonv1.qip
 set_global_assignment -name SDC_FILE ${boardpath}/chameleon64/constraints.sdc
-set_global_assignment -name VHDL_FILE ${boardpath}/Board/chameleon64/gen_reset.vhd
-set_global_assignment -name VHDL_FILE ${boardpath}/RTL/Video/video_vga_dither.vhd
-set_global_assignment -name VERILOG_FILE ${boardpath}/RTL/Sound/hybrid_pwm_sd.v
+set_global_assignment -name VHDL_FILE ${boardpath}/chameleon64/gen_reset.vhd
 
