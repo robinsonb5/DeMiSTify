532be38dab1f5ccdf72edbabeae59386ef9ba01b
Alastair M. Robinson <robinsonb5@gmail.com>
2021-10-27 20:14:18 +0100

    Added SDRAM defs for MiST, added missing defs to config_pkg

--- templates/demistify_config_pkg.vhd
+++ templates/demistify_config_pkg.vhd
@@ -9,6 +9,26 @@ constant demistify_romspace : integer := 14; -- 16k address space to accommodate
 constant demistify_romsize1 : integer := 13; -- 8k fot the first chunk
 constant demistify_romsize2 : integer := 12; -- 4k for the second chunk, mirrored across the last 4k
 
+-- Core-specific button mapping.
+-- Joysticks are (currently) 8 bits width, with the directions in the lower four bits.
+-- Button 1 is the main fire button, Button 2 is the secondary fire button to the right of button 1
+-- Button 3 is a third (possibly less conveniently placed) button 
+-- Button 4 is mapped as a "start" or "run" button.
+
+-- By adjusting the following constants you can assign each button a place in the joystick bitmap.
+-- The Megadrive core, for instance, expects the following bitmap:
+-- start(7) C(6) B(5) A(4) directions(3:0)
+-- Since Megadrive button B is the most important, we map button 1 -> B, button 2 -> C, button 3 -> A
+-- and button 4 -> Start, hence button1 = 5, button2 = 6, button3 = 4 and button4 = 7,
+
+constant demistify_button1 : integer := 4;
+constant demistify_button2 : integer := 5;
+constant demistify_button3 : integer := 6;
+constant demistify_button4 : integer := 7;
+
+constant demistify_serialdebug : std_logic := '0';
+
+
 	-- Declare the guest component
 	
 	COMPONENT guest_mist -- Rename to match the guest core
