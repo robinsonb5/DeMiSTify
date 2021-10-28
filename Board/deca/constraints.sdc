e69fec38499957a796b0310cce827c3389664506
somhi <somhix@gmail.com>
2021-10-28 23:23:10 +0200

    changes in board deca constraints.sdc and support.tcl

--- Board/deca/constraints.sdc
+++ Board/deca/constraints.sdc
@@ -1,6 +1,9 @@
 create_clock -name {clk_50} -period 20.000 -waveform {0.000 10.000} { MAX10_CLK1_50 }
 create_generated_clock -name spiclk -source [get_ports {MAX10_CLK1_50}] -divide_by 4 [get_registers {substitute_mcu:controller|spi_controller:spi|sck}]
 
+set hostclk { clk_50 }
+set supportclk { clk_50 }
+
 derive_pll_clocks -create_base_clocks
 derive_clock_uncertainty
 
--- Board/deca/deca_support.tcl
+++ Board/deca/deca_support.tcl
@@ -6,3 +6,7 @@ set_global_assignment -name VHDL_FILE ${boardpath}/deca/audio/audio_i2s.vhd
 set_global_assignment -name VERILOG_FILE ${boardpath}/deca/hdmi/I2C_Controller.v
 set_global_assignment -name VERILOG_FILE ${boardpath}/deca/hdmi/I2C_HDMI_Config.v
 #set_global_assignment -name QIP_FILE ${boardpath}/deca/pll/pll2.qip
+#
+​set_global_assignment -name VERILOG_MACRO "DEMISTIFY_PARALLEL_AUDIO=1"
+
+
