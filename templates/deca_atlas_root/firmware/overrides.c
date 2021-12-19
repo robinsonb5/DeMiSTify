c28e5c82e44f7f45b5ca74f1ef531935d9bf25c3
somhi <somhix@gmail.com>
2021-12-20 00:10:46 +0100

    deca&atlas overrides.c template

--- templates/deca_atlas_root/firmware/overrides.c
+++ templates/deca_atlas_root/firmware/overrides.c
@@ -34,3 +34,21 @@ char *autoboot()
 	return(result);
 }
 
+
+/*    worked for BBC-micro, methods above didn't
+
+#include "diskimg.h"
+
+const char *bootvhd_name="BBC     VHD";
+const char *bootrom_name="BBC     ROM";
+
+char *autoboot()
+{
+    char *result=0;
+    diskimg_mount(bootvhd_name,0);
+	if(!LoadROM(bootrom_name))
+		result="ROM loading failed";
+    return(result);
+}
+*/
+
